a636ed033f444ad027a3c1917d726da1fddca315
JunjieMei <mmeijunjie@gmail.com>
2024-11-21 22:28:25 +0800

    tj

---
+++ array/src/main/java/org/example/ArrayOriginalCodeTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class ArrayOriginalCodeTest {
+
+    public static void main(String[] args) {
+
+        ArrayList<Long> arrayList = new ArrayList<>();
+
+        for(int i=0;i<12;i++){
+            arrayList.add(Long.valueOf(i));
+        }
+    }
+}
